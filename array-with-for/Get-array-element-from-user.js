03f7772c9ff80cbd085ce213fd5461e6f914edaa
karthikeyani-r787 <karthianjammalram787@gmail.com>
2026-08-16 15:55:37 +0530

    add search for an element

---
+++ array-with-for/Get-array-element-from-user.js
@@ -0,0 +1,8 @@
+//Task : get input from user and add it to the array
+const prompt=require("prompt-sync")();
+let array=[];
+let n=prompt("Enter the length of the array : ");
+for (let i=0;i<n;i++){
+    array[i]=prompt(`Enter array value at index ${i} : `)
+}
+console.log(`your array is [${array}]`)
