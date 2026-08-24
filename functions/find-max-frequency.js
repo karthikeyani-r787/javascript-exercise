226accd427558641da2cfb66389c58d615a95a08
karthikeyani-r787 <karthianjammalram787@gmail.com>
2026-08-24 19:24:43 +0530

    Add to find maximum frequency

---
+++ functions/find-max-frequency.js
@@ -0,0 +1,19 @@
+//Task : Find the Most Frequent Number
+let numbers = [10, 20, 10, 30, 20, 10, 40,40,40,40, 20];
+function mostFrequent(arr){
+    let max=0,mostfrequent;
+    for(let i=0;i<arr.length;i++){
+        let count=0;
+        for(let j=i;j<arr.length;j++){
+          if(arr[i]===arr[j]){
+            count++;
+          }  
+        }
+        if(count>max){
+            max=count;
+            mostfrequent=arr[i]
+        }
+    }
+    return mostfrequent;
+}
+console.log(mostFrequent(numbers));
